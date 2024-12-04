94e941fcb43740eec478ab351fcb238433d29ca9
Kang San KIM <119637398+yoojadoni@users.noreply.github.com>
2024-12-04 15:08:39 +0900

    Update NettyConfiguration.java

--- kr/co/nettyProject/netty/config/NettyConfiguration.java
+++ kr/co/nettyProject/netty/config/NettyConfiguration.java
@@ -2,3 +2,16 @@
 
 @Configuration
 @RequiredArgsConstructor
+public class NettyConfiguration {
+  @Value("${tcp.server.port}")
+  private int port;
+  @Value("${tcp.server.workerCount}")
+  private int workerCount;
+  @Value("${tcp.server.backlog}")
+  private int backlog;
+
+  @Bean
+  public ServerBootstrap serverBootstrap(NettyChannelInitializer nettyChannelInitializer){
+
+  }
+}
